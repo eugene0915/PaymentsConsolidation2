a7aa6618b10ace0b576625ff714fc63d4160ae99
eugene0915 <dryujin0715@gmail.com>
2023-03-03 13:09:43 +0900

    background css

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,19 @@ import Link from "next/link"
 const Home = () => {
   return (
     <>
-      <h1 className="text-pink-400">payments</h1>
-      <div className="flex justify-center	gap-10 align-middle h-screen">
+    <div className="wholepage bg-gray-200">
+      <h1 className="text-gray-500 font-bold text-xl justify-end flex mr-10 pt-8">payments</h1>
+      <div className=''>
+      <div className="flex justify-center	gap-10 ">
         <button className="bg-blue-400 "><Link href="/toss">toss</Link></button>
         <button><Link href="/paypal">paypal</Link></button>
         <button><Link href="/kakaopay">kakao</Link></button>
         <button><Link href="/alipay">alipay</Link></button>
         <button>wechat pay</button>
       </div>
-      <button clssName="dbtn">test</button>
+      </div>
+      </div>
+      
     </>
   )
 }
