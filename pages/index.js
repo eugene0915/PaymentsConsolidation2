4366cd602eb26dda2303b3dabef7ba7d931f16d5
eugene0915 <dryujin0715@gmail.com>
2023-03-09 14:34:27 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <>
     <div className="wholepage bg-gray-200">
-      <h1 className="text-gray-500 font-bold text-xl justify-end flex mr-10 pt-8">payments</h1>
+      <h1 className="text-gray-500 font-bold text-xl justify-end flex mr-10 pt-8">paymentss</h1>
       <div className=''>
       <div className="flex justify-center	gap-10 ">
         <button className="bg-blue-400 "><Link href="/toss">toss</Link></button>
